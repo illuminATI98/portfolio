032a9f056f556bbbb9b68bafea19c3989f98bcef
Attila Dobi <attiladobi100@gmail.com>
2023-07-29 18:16:37 +0200

    Add links and styles to Navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { React , useState } from 'react'
 import "./Navbar.scss";
+import { images } from "../../constants";
+import {HiMenu, HiX} from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className='app__navbar-links'>
+        {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className='app__navbar-menu'>
+        <HiMenu onClick={() => setToggle(true)}/>
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{duration: 0.85, ease: "easeOut"}}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul className='app__navbar-links'>
+              {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
+                <li key={`link-${item}`}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                </li>
+                ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
+    </nav>
   )
 }
 
