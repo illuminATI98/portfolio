1b346520ff283b654ee0f7f2c80bc7ce4aaf93c9
Attila Dobi <attiladobi100@gmail.com>
2023-07-31 19:45:31 +0200

    Add projectId and token to sanity client config

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_ID,
+  projectId: "08uzq2pr",
   dataset: "production",
   apiVersion: "2023-07-30",
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN
+  token: "skNJTTF6NG8QefKbhTZ6TtnZaec2TFiYm7oVSdCuyk0wPAOQWBlkf8inXNM8yfrwcOQQCUeBD1SkIRucUc3vHPfo287vzptUVJAOCfHKrM5Jar1prPELkGe08PnQMbTNqtRFewXrZ8sz99zKmhc3xSNCHgS4nARHqBEBbueFk88i0QzH1WoK"
 });
 
 const builder = imageUrlBuilder(client);
