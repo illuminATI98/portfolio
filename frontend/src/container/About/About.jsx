500233364aa7f84908722adfb3d591ae9336e00e
Attila Dobi <attiladobi100@gmail.com>
2023-07-31 13:33:38 +0200

    Fix navigation bug and implement wrapper

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './About.scss';
 import { motion } from "framer-motion";
+import { AppWrap } from '../../wrapper';
 
 const About = () => {
   return (
-    <div id='about' className='app__about app__flex'>
+    <div className='app__about app__flex'>
       <motion.div
         whileInView={{x: [-100,0], opacity: [0,1]}}
         transition={{duration: 3}}
@@ -22,8 +23,8 @@ const About = () => {
         className='app__about-info'
       >
         <div className='app__about-description'>
-          <p className='p-text'>
-          During my CodeCool journey, I have gained a solid foundation in software development principles, including proficiency in programming languages such as JavaScript, React.js, C# and familiarity with the .NET framework. I have acquired hands-on experience with various tools and technologies used in the field, including JetBrains Rider, Visual Studio and other development environments. 
+          <p className='bold-text'>
+          During my <span>CodeCool</span> journey, I have gained a solid foundation in software development principles, including proficiency in programming languages such as <span>JavaScript, React.js, C#</span> and familiarity with the <span>.NET framework</span>. I have acquired hands-on experience with various tools and technologies used in the field, including <span>JetBrains Rider, Visual Studio</span> and other development environments. 
           </p>
         </div>
         <div className='circle-codecool'>
@@ -33,4 +34,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, "about")
--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Header.scss';
 import { motion } from "framer-motion";
-import { images } from "../../constants"
+import { images } from "../../constants";
+import { AppWrap } from "../../wrapper";
 
 const scaleVariants = {
   whileInView: {
@@ -17,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div id='home' className='app__header app__flex'>
+    <div className='app__header app__flex'>
       <motion.div
         whileInView={{x: [-100,0], opacity: [0,1]}}
         transition={{duration: 3}}
@@ -69,4 +70,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header, "home")
