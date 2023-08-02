609f1be1f5d73940789feba0810c03c4fc5ce1b4
Attila Dobi <attiladobi100@gmail.com>
2023-08-02 18:38:29 +0200

    Import token and id from .env

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: "08uzq2pr",
+  projectId: import.meta.env.VITE_APP_SANITY_ID,
   dataset: "production",
   apiVersion: "2023-07-30",
   useCdn: true,
-  token: "skNJTTF6NG8QefKbhTZ6TtnZaec2TFiYm7oVSdCuyk0wPAOQWBlkf8inXNM8yfrwcOQQCUeBD1SkIRucUc3vHPfo287vzptUVJAOCfHKrM5Jar1prPELkGe08PnQMbTNqtRFewXrZ8sz99zKmhc3xSNCHgS4nARHqBEBbueFk88i0QzH1WoK"
+  token: import.meta.env.VITE_APP_SANITY_TOKEN
 });
 
 const builder = imageUrlBuilder(client);
